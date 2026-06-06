246fd8153c525a913cd39fcc421c01a83cfce98c
Achyut Parajuli <achyutp10@gmail.com>
2026-06-06 21:05:32 +0545

    CRUD

--- untitled/src/DemoJDBC.java
+++ untitled/src/DemoJDBC.java
@@ -37,6 +37,137 @@
 //}
 
 import javax.swing.plaf.nimbus.State;
+//import  java.sql.*;
+//import java.util.ArrayList;
+//
+//public class DemoJDBC {
+//    public static void main(String[] args) throws  Exception {
+//        /*
+//            import package
+//            load and register
+//            create connection
+//            create statement
+//            execute statement
+//            process the result
+//            close
+//         */
+//        String url = "jdbc:postgresql://localhost:5432/demo";
+//        String uname = "postgres";
+//        String pass = "achyutp10";
+//        String sql = "select * from student";
+//
+//        Class.forName("org.postgresql.Driver");
+//        Connection con = DriverManager.getConnection(url, uname, pass);
+//        System.out.println("Connection Established");
+//
+//
+//        Statement st = con.createStatement();
+//        ResultSet rs = st.executeQuery(sql);
+//        ArrayList arr = new ArrayList();
+//        while (rs.next()) {
+//            System.out.print(rs.getInt(1)+ " - ");
+//            System.out.print(rs.getInt(2)+ " - ");
+//            System.out.println(rs.getString(3));
+//
+//        }
+//
+//
+//        con.close();
+//        System.out.println("Connection closed");
+//
+//
+//    }
+//}
+
+//
+//import  java.sql.*;
+//import java.util.ArrayList;
+//
+//public class DemoJDBC {
+//    public static void main(String[] args) throws  Exception {
+//        /*
+//            import package
+//            load and register
+//            create connection
+//            create statement
+//            execute statement
+//            process the result
+//            close
+//         */
+//        String url = "jdbc:postgresql://localhost:5432/demo";
+//        String uname = "postgres";
+//        String pass = "achyutp10";
+//        String sql = "Insert into student(sid, sname, marks) values (22, 'John', 55)";
+//
+//        Class.forName("org.postgresql.Driver");
+//        Connection con = DriverManager.getConnection(url, uname, pass);
+//        System.out.println("Connection Established");
+//
+//
+//        Statement st = con.createStatement();
+//        boolean result = st.execute(sql);
+//        if (result) {
+//            System.out.println("Data inserted");
+//        } else {
+//            System.out.println("Data not inserted");
+//
+//        }
+//
+//
+//
+//        con.close();
+//        System.out.println("Connection closed");
+//
+//
+//    }
+//}
+
+
+
+//
+//import  java.sql.*;
+//import java.util.ArrayList;
+//
+//public class DemoJDBC {
+//    public static void main(String[] args) throws  Exception {
+//        /*
+//            import package
+//            load and register
+//            create connection
+//            create statement
+//            execute statement
+//            process the result
+//            close
+//         */
+//        String url = "jdbc:postgresql://localhost:5432/demo";
+//        String uname = "postgres";
+//        String pass = "achyutp10";
+//        String sql = "Update student set sname='Max' where sid = 22";
+//
+//        Class.forName("org.postgresql.Driver");
+//        Connection con = DriverManager.getConnection(url, uname, pass);
+//        System.out.println("Connection Established");
+//
+//
+//        Statement st = con.createStatement();
+//        boolean result = st.execute(sql);
+//        if (result) {
+//            System.out.println("Data updated");
+//        } else {
+//            System.out.println("Data not updated");
+//
+//        }
+//
+//
+//
+//        con.close();
+//        System.out.println("Connection closed");
+//
+//
+//    }
+//}
+
+
 import  java.sql.*;
 import java.util.ArrayList;
 
@@ -54,7 +185,7 @@ public class DemoJDBC {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "achyutp10";
-        String sql = "select * from student";
+        String sql = "delete from student where sid = 22";
 
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
@@ -62,14 +193,14 @@ public class DemoJDBC {
 
 
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(sql);
-        ArrayList arr = new ArrayList();
-        while (rs.next()) {
-            System.out.print(rs.getInt(1)+ " - ");
-            System.out.print(rs.getInt(2)+ " - ");
-            System.out.println(rs.getString(3));
+        boolean result = st.execute(sql);
+        if (result) {
+            System.out.println("Data deleted");
+        } else {
+            System.out.println("Data not deleted");
 
         }
+
 
 
         con.close();
@@ -78,4 +209,3 @@ public class DemoJDBC {
 
     }
 }
-
