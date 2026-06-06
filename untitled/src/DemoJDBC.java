7086937968dc292ef552e903df55521296326d08
Achyut Parajuli <achyutp10@gmail.com>
2026-06-06 21:46:35 +0545

    CRUD

--- untitled/src/DemoJDBC.java
+++ untitled/src/DemoJDBC.java
@@ -167,9 +167,55 @@ import javax.swing.plaf.nimbus.State;
 //    }
 //}
 
+////
+//import  java.sql.*;
+//import java.util.ArrayList;
+//
+//public class DemoJDBC {
+//    public static void main(String[] args) throws  Exception {
+//        /*
+//            import package
+//            load and register
+//            create connection
+//            create statement
+//            execute statement
+//            process the result
+//            close
+//         */
+//        String url = "jdbc:postgresql://localhost:5432/demo";
+//        String uname = "postgres";
+//        String pass = "achyutp10";
+//        String sql = "delete from student where sid = 22";
+//
+//        Class.forName("org.postgresql.Driver");
+//        Connection con = DriverManager.getConnection(url, uname, pass);
+//        System.out.println("Connection Established");
+//
+//
+//        Statement st = con.createStatement();
+//        boolean result = st.execute(sql);
+//        if (result) {
+//            System.out.println("Data deleted");
+//        } else {
+//            System.out.println("Data not deleted");
+//
+//        }
+//
+//
+//
+//        con.close();
+//        System.out.println("Connection closed");
+//
+//
+//    }
+//}
+
+
+// Prepared statement
 
 import  java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DemoJDBC {
     public static void main(String[] args) throws  Exception {
@@ -182,22 +228,36 @@ public class DemoJDBC {
             process the result
             close
          */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter sid: ");
+        int sid = (int) (sc.nextInt());
+        sc.nextLine();
+        System.out.println("Enter sname: ");
+        String sname = sc.nextLine();
+        System.out.println("Enter Marks: ");
+        int marks = (int) (sc.nextInt());
+        sc.nextLine();
+
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "achyutp10";
-        String sql = "delete from student where sid = 22";
+        String sql = "Insert into student values(?,?,?)";
 
         Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
         System.out.println("Connection Established");
 
 
-        Statement st = con.createStatement();
-        boolean result = st.execute(sql);
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, sid);
+        ps.setInt(2, marks);
+        ps.setString(3, sname);
+
+        boolean result = ps.execute();
         if (result) {
-            System.out.println("Data deleted");
+            System.out.println("Data inserted");
         } else {
-            System.out.println("Data not deleted");
+            System.out.println("Data not inserted");
 
         }
 
