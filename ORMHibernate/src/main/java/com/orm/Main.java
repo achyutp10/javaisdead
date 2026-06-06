af85b5766c34b35d48d0a6f675d4bb7afa82bb20
Achyut Parajuli <achyutp10@gmail.com>
2026-06-06 22:24:47 +0545

    Hibernate ORM Create

--- ORMHibernate/src/main/java/com/orm/Main.java
+++ ORMHibernate/src/main/java/com/orm/Main.java
@@ -1,7 +1,28 @@
 package com.orm;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Student s1 = new Student();
+        s1.setsName("Achyut");
+        s1.setRollNo(101);
+        s1.setsAge(25);
+
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(com.orm.Student.class);
+        cfg.configure();
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+        session.persist(s1);
+        transaction.commit();
+        System.out.println(s1);
+
     }
 }
