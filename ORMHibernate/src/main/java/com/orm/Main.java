1e9dc4b602177393eb4a4fca2981229045d47013
Achyut Parajuli <achyutp10@gmail.com>
2026-06-07 09:26:31 +0545

    update & delete data

--- ORMHibernate/src/main/java/com/orm/Main.java
+++ ORMHibernate/src/main/java/com/orm/Main.java
@@ -36,8 +36,82 @@
 //
 //    }
 //}
+//
+//// fetch data
+//package com.orm;
+//
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.Transaction;
+//import org.hibernate.cfg.Configuration;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Student s1 = new Student();
+//        s1.setsName("Achyut3");
+//        s1.setRollNo(106);
+//        s1.setsAge(25);
+//
+//        Student s2 = null;
+//
+////        Configuration cfg = new Configuration();
+////        cfg.addAnnotatedClass(com.orm.Student.class);
+////        cfg.configure();
+////        SessionFactory sf = cfg.buildSessionFactory();
+//
+//        SessionFactory sf = new Configuration().
+//                addAnnotatedClass(com.orm.Student.class)
+//                .configure().
+//                buildSessionFactory();
+//
+//        Session session = sf.openSession();
+//
+//        s2 = session.find(Student.class,102);
+////        s2 = session.load(Student.class, 102);
+//
+//        session.close();
+//        sf.close();
+//        System.out.println(s2);
+//
+//    }
+//}
+//
+//
+//// Update data
+//package com.orm;
+//
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.Transaction;
+//import org.hibernate.cfg.Configuration;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Student s1 = new Student();
+//        s1.setsName("Abisha");
+//        s1.setRollNo(102);
+//        s1.setsAge(23);
+//
+//        SessionFactory sf = new Configuration().
+//                addAnnotatedClass(com.orm.Student.class)
+//                .configure().
+//                buildSessionFactory();
+//
+//        Session session = sf.openSession();
+//        Transaction transaction = session.beginTransaction();
+//        session.merge(s1);
+//        transaction.commit();
+//
+//        session.close();
+//        sf.close();
+//        System.out.println(s1);
+//
+//    }
+//}
 
-// fetch data
+
+
+// Delete data
 package com.orm;
 
 import org.hibernate.Session;
@@ -48,16 +122,9 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.setsName("Achyut3");
-        s1.setRollNo(106);
-        s1.setsAge(25);
-
-        Student s2 = null;
-
-//        Configuration cfg = new Configuration();
-//        cfg.addAnnotatedClass(com.orm.Student.class);
-//        cfg.configure();
-//        SessionFactory sf = cfg.buildSessionFactory();
+//        s1.setsName("Abisha");
+//        s1.setRollNo(102);
+//        s1.setsAge(23);
 
         SessionFactory sf = new Configuration().
                 addAnnotatedClass(com.orm.Student.class)
@@ -65,13 +132,15 @@ public class Main {
                 buildSessionFactory();
 
         Session session = sf.openSession();
+        s1 = session.find(Student.class, 106);
+        Transaction transaction = session.beginTransaction();
 
-        s2 = session.find(Student.class,102);
-//        s2 = session.load(Student.class, 102);
+        session.remove(s1);
+        transaction.commit();
 
         session.close();
         sf.close();
-        System.out.println(s2);
+        System.out.println(s1);
 
     }
 }
