31baeee7c4342ae8c9eaf78b8fb0bcc0a077d372
Achyut Parajuli <achyutp10@gmail.com>
2026-06-07 09:12:35 +0545

    Refactor configuration

--- ORMHibernate/src/main/java/com/orm/Main.java
+++ ORMHibernate/src/main/java/com/orm/Main.java
@@ -8,20 +8,27 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
-        s1.setsName("Achyut");
-        s1.setRollNo(101);
+        s1.setsName("Achyut3");
+        s1.setRollNo(106);
         s1.setsAge(25);
 
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(com.orm.Student.class);
-        cfg.configure();
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(com.orm.Student.class);
+//        cfg.configure();
+//        SessionFactory sf = cfg.buildSessionFactory();
 
-        SessionFactory sf = cfg.buildSessionFactory();
+        SessionFactory sf = new Configuration().
+                addAnnotatedClass(com.orm.Student.class)
+                .configure().
+                buildSessionFactory();
+
         Session session = sf.openSession();
 
         Transaction transaction = session.beginTransaction();
         session.persist(s1);
         transaction.commit();
+        session.close();
+        sf.close();
         System.out.println(s1);
 
     }
