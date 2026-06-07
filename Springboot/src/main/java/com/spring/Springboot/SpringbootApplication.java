632b863a5c051c6e727d12bcf9cd1c7a3a42995c
Achyut Parajuli <achyutp10@gmail.com>
2026-06-07 19:55:00 +0545

    Spring Boot DI

--- Springboot/src/main/java/com/spring/Springboot/SpringbootApplication.java
+++ Springboot/src/main/java/com/spring/Springboot/SpringbootApplication.java
@@ -2,13 +2,22 @@ package com.spring.Springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-		System.out.println("hello World");
+		ApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
+
+//		Alien obj = context.getBean(Alien.class);
+//		obj.code();
+//
+//		Alien obj1 = context.getBean(Alien.class);
+//		obj1.code();
+
+		Laptop obj1 = context.getBean(Laptop.class);
+		obj1.compile();
 	}
 
 }
