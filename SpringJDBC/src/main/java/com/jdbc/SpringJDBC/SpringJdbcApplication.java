5cde7b22035c1645e57d12796572bcfd34b4b9ba
Achyut Parajuli <achyutp10@gmail.com>
2026-06-12 11:43:08 +0545

    Spring jdbc connection and execution

--- SpringJDBC/src/main/java/com/jdbc/SpringJDBC/SpringJdbcApplication.java
+++ SpringJDBC/src/main/java/com/jdbc/SpringJDBC/SpringJdbcApplication.java
@@ -1,14 +1,30 @@
 package com.jdbc.SpringJDBC;
 
+import com.jdbc.SpringJDBC.model.Student;
+import com.jdbc.SpringJDBC.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcApplication.class, args);
-		System.out.println("JDBC Spring");
+		ApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+		Student s = context.getBean(Student.class);
+		s.setRollNo(104);
+		s.setName("Achyut");
+		s.setMarks(78);
+
+		StudentService service = context.getBean(StudentService.class);
+
+		service.addStudent(s);
+
+		List<Student> students = service.getStudents();
+		System.out.println(students);
+		}
 	}
 
-}
+
---
+++ SpringJDBC/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollno, name, marks) values (101, 'Kiran', 79);
+insert into student (rollno, name, marks) values (102, 'Harsh', 59);
+insert into student (rollno, name, marks) values (103, 'Kushal', 89);
